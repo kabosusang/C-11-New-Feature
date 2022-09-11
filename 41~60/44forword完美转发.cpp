6eda4852b7e07f10ce39fc02c16a9568bc4caf1c
Ka_bo <87074561+kabosusang@users.noreply.github.com>
2022-09-11 20:59:50 +0800

    Add files via upload

--- 41~60/44forword完美转发.cpp
+++ 41~60/44forword完美转发.cpp
@@ -27,7 +27,7 @@ void testForward(T && v)//未定义类型的引用
 {
     printValue(v);
     printValue(move(v));
-    printValue(forward<T>(v));  //这个v看前面的那个T的类型 T是左值引用v就是左值
+    printValue(forward<T>(v));  9//这个v看前面的那个T的类型 T是左值引用v就是左值
     cout << endl;
 }
 
@@ -35,10 +35,12 @@ int main(void)
 {
     testForward(520);//右值引用 推导出1.printValue(v)为左值 2.printValue(move(v))为右值
     //3.printValue(forward<T>(v));
-
+    //当T为左值引用类型时，u将被转化成T类型的左值引用。
+    //当T为左值时，u将被转化成T类型的右值。
+    //当T为右值时，可以不考虑这种情况
     int num = 1314;
     testForward(num);
-    testForward(forward<int>(num));
+    testForward(forward<int>(num));//因为int不是左值引用 所以num是左值 forward只能决定传入的num是左值还是右值
     testForward(forward<int&>(num));
     testForward(forward<int&&>(num));
 
