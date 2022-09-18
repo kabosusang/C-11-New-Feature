88ec4b0ff08a21c3a3012c173e94041d2ed47ac7
Ka_bo <87074561+kabosusang@users.noreply.github.com>
2022-09-18 21:20:26 +0800

    Add files via upload

---
+++ 侯捷c++11/1variadicTemplates.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+using namespace std;
+void printX()
+{
+
+}
+template<typename T, typename... Types> //主要用来递归
+void printX(const T& firstArg, const Types&... args)
+{
+     cout << "调用了特化模板" << endl;
+    cout<< firstArg <<endl; //printf first argument
+    printX(args...);    //call print() for remaining arguments
+} // sizeof...(args) 算出这个pack有多少个
+template<typename...Types>
+void printX(const Types&... args)
+{
+    cout << "调用了泛化模板" << endl;
+    
+     printX(args...);
+}
+
+int main()
+{
+    printX(123,"Hello","12345",23.23,bitset<16>(1234));
+    return 0;
+}
